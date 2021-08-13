624a04031d4fe3240fd81f8c4e40e1eb7c9501f7
JoseMBetancourt <josebetancourt@unicauca.edu.co>
2021-08-13 13:53:33 -0500

    random color button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var randomButton = document.getElementById("random");
 var bg = document.getElementById("bg");
 
 function changeColor() {
@@ -9,6 +10,18 @@ function changeColor() {
   css.textContent = bg.style.background + ";";
 }
 
+const genRanHex = (size) =>
+  [...Array(size)]
+    .map(() => Math.floor(Math.random() * 16).toString(16))
+    .join("");
+
+function randomColors() {
+  color1.value = "#" + genRanHex(6);
+  color2.value = "#" + genRanHex(6);
+  changeColor();
+}
 color1.addEventListener("input", changeColor);
 
 color2.addEventListener("input", changeColor);
+
+randomButton.addEventListener("click", randomColors);
